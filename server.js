96b40a76e5816eb18af82ea1258d320cbb99a58d
André Santana <109093157+AndreSanLopes@users.noreply.github.com>
2026-03-20 15:18:37 -0300

    Primeiros passos

---
+++ server.js
@@ -0,0 +1,11 @@
+import { createServer } from "node:http";
+//request = obtém dados da requisição que o usuário faz ao servidor
+//response = objeto para devolver a resposta para o usuário 
+const server = createServer( (request, response)=> {
+    response.write('Essa e uma mensagem do servidor')
+    return response.end()
+})
+
+/*chamar utilizando uma porta*/
+//a porta irá servir para não ter conflitos com outros projetos
+server.listen(3333)
